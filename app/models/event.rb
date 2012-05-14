530071768597ede01867f07ba780a56298fd362b
Paul Nossoff <kaluzhanin@gmail.com>
2012-05-14 20:14:01 +0400

    added pacha and space

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,13 +5,27 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :places
   accepts_nested_attributes_for :price
 
+  default_scope order: "date ASC"
+
   def self.fetch
   	data = ClubParser.fetch_all_from :amnesia
-  	puts data
   	if data
 	  	data[:amnesia][:events].each do |event|
 	  		Event.create!(event)
 	  	end
   	end
+    data = ClubParser.fetch_all_from :pacha
+    if data
+      data[:pacha][:events].each do |event|
+        Event.create!(event)
+      end
+    end
+    data = ClubParser.fetch_all_from :space
+    puts data.inspect
+    if data
+      data[:space][:events].each do |event|
+        Event.create!(event)
+      end
+    end
   end
 end
