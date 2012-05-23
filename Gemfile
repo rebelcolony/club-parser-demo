156c924282831f8eb8d3456d258af5824d7589b5
Paul Nossoff <kaluzhanin@gmail.com>
2012-05-23 21:18:40 +0400

    six clubs support now

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.2.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-gem 'club-parser', '~> 0.1.0.alpha', git: "git@github.com:ivliaspiration/club-parser.git"
+gem 'club-parser', '~> 0.1.6.alpha', git: "git@github.com:ivliaspiration/club-parser.git"
 gem 'delayed_job_active_record'
 
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -8,12 +8,12 @@ class Event < ActiveRecord::Base
   default_scope order: "date ASC"
 
   def self.fetch
-  	data = ClubParser.fetch_all_from :amnesia
-  	if data
-	  	data[:amnesia][:events].each do |event|
-	  		Event.create!(event)
-	  	end
-  	end
+  	# data = ClubParser.fetch_all_from :amnesia
+  	# if data
+	  # 	data[:amnesia][:events].each do |event|
+	  # 		Event.create!(event)
+	  # 	end
+  	# end
     data = ClubParser.fetch_all_from :pacha
     if data
       data[:pacha][:events].each do |event|
@@ -21,9 +21,26 @@ class Event < ActiveRecord::Base
       end
     end
     data = ClubParser.fetch_all_from :space
-    puts data.inspect
     if data
       data[:space][:events].each do |event|
+        Event.create!(event)
+      end
+    end
+    data = ClubParser.fetch_all_from :esparadis
+    if data
+      data[:esparadis][:events].each do |event|
+        Event.create!(event)
+      end
+    end
+    data = ClubParser.fetch_all_from :privilege
+    if data
+      data[:privilege][:events].each do |event|
+        Event.create!(event)
+      end
+    end
+    data = ClubParser.fetch_all_from :eden
+    if data
+      data[:eden][:events].each do |event|
         Event.create!(event)
       end
     end
